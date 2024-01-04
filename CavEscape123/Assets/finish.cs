c1ee6b3b378f42ff54291319de6e2de0a0cbc780
Kanoten <oskar.forslund@icloud.com>
2024-01-04 17:09:03 +0100

    slut

---
+++ CavEscape123/Assets/finish.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finish : MonoBehaviour
+{
+    private AudioSource finishSound;
+    private bool end = true;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player" && end)
+        {
+            end = false;
+            finishSound.Play();
+            Invoke("CompleateLevel", 1f);
+        }
+    }
+
+    private void CompleateLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
